a4aeee7c96875ba3fd76753affd8227bb96a0267
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-09-25 19:35:09 +0900

    최신 2023-09-25

--- my-bicycle/KakaoComponent.js
+++ my-bicycle/KakaoComponent.js
@@ -1,15 +1,50 @@
-// 카카오 api 연동 컴포넌트
-import React from 'react';
+import React, { useEffect } from 'react';
+import { PermissionsAndroid } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-const apiKey = "180de339da461cd47b488bce6adeca83";
-const uri = `https://map.kakao.com/?apikey=${apiKey}`;
+const uri = `https://806hyogi.github.io/hostingtest/`;
+
+// 위치 권한 요청 함수
+async function requestLocationPermission() {
+    try {
+        // 안드로이드 위치 접근 권한 요청
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+                title: "위치권한알림",
+                message:
+                    "위치 권한을 허용하시겠습니까",
+                buttonNegative: "취소",
+                buttonPositive: "확인"
+            }
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log("You can use the location");
+        } else {
+            console.log("Location permission denied");
+        }
+    } catch (err) {
+        console.warn(err);
+    }
+}
 
 export default function WebViewComponent({ style }) {
+
+    // 처음 화면에 나타날 때 위치 권한 요청 함수 호출
+    useEffect(() => {
+        requestLocationPermission();
+    }, []);
+
     return (
-        <WebView
+        // 웹뷰로 페이지 로드
+        <WebView 
             style={style}
             source={{ uri }}
+            
+            // 웹뷰에서 Geolocation API 호출하여 항상 위치 접근 허용
+            geolocationEnabled={true}
+            onGeolocationPermissionsShowPrompt={(origin, callback) => callback(true)}
+            
         />
     );
 }
